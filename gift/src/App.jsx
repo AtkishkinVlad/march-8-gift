a37966fcd54321b89a25512ecc987776d1279315
atkvs <vladatkishkin@vk.com>
2024-02-25 00:37:57 +0500

    feat(gift): revert content

---
+++ gift/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='content'>
+      <header className='header'>
+        <h1 className='title'>
+          8 Марта<br />
+          с Диадоком
+        </h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
