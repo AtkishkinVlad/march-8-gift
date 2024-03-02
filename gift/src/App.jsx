418ab449b937926ceed37463113e94af5342653e
atkvs <vladatkishkin@vk.com>
2024-03-02 23:22:02 +0500

    add footer

--- gift/src/App.jsx
+++ gift/src/App.jsx
@@ -4,15 +4,20 @@ import { Congratulations } from './components/Congratulations';
 import { Header } from './components/Header';
 import { SeparatorBlock } from './components/SeparatorBlock';
 import { Cards } from './components/Cards';
+import { Icons } from './components/Icons';
+import { Footer } from './components/Footer';
 
 function App() {
-  
   return (
     <div className='content'>
+      <Icons />
       <Header />
-      <Congratulations />
-      <SeparatorBlock />
-      <Cards />
+      <main>
+        <Congratulations />
+        <SeparatorBlock />
+        <Cards />
+      </main>
+      <Footer />
     </div>
   )
 }
