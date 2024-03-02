ec427e3d071e1f3adf2e27234939a87561015b26
atkvs <vladatkishkin@vk.com>
2024-03-02 17:12:15 +0500

    separator block change text

--- gift/src/components/SeparatorBlock.jsx
+++ gift/src/components/SeparatorBlock.jsx
@@ -1,7 +1,7 @@
 export const SeparatorBlock = () => (
     <figure className='figure'>
         <figcaption className='figcaption'>
-          Давай посмотрим на мои пожелания в трех карточках :)  
+          Нажми на карточки чтобы узнать что я желаю тебе сегодня :) 
         </figcaption>
         <img src='https://kontur.ru/Files/Modules/LandingPage/5675i/4bcca3b9-ef92-463e-ba7d-ca6399d1c714.svg' className='arrow' />
       </figure>
