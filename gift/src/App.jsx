555fdf4f21d09da832cbe338fa48928837656471
atkvs <vladatkishkin@vk.com>
2024-03-02 14:00:54 +0500

    feat(gift): add new components

--- gift/src/App.jsx
+++ gift/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 
+import { Congratulations } from './components/Congratulations';
+import { Header } from './components/Header';
+
 function App() {
   return (
     <div className='content'>
-      <header className='header'>
-        <h1 className='title'>
-          8 Марта<br />
-          с Диадоком
-        </h1>
-      </header>
+      <Header />
+      <Congratulations />
+      <figure className='figure'>
+        <figcaption className='figcaption'>
+          Давай посмотрим на мои пожелания в трех карточках :)  
+        </figcaption>
+        <img src='https://kontur.ru/Files/Modules/LandingPage/5675i/4bcca3b9-ef92-463e-ba7d-ca6399d1c714.svg' className='arrow' />
+      </figure>
     </div>
   )
 }
---
+++ gift/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => (
+    <header className='header'>
+        <img className='logo' src='https://s.kontur.ru/common-v2/logos/logo-diadoc-32.svg' alt="Логотип Диадока" />
+        <h1 className='title'>
+          8 Марта<br/>
+          с{'\u00A0'}Диадоком
+        </h1>
+    </header>
+)
